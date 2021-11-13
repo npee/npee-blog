f6c1d4edf2f13da7292c6977773768e9237be5b4
npee <jmc4664@gmail.com>
2021-11-14 03:35:30 +0900

    Initial Commit

---
+++ src/main/java/io/npee/blog/SpringbootBlogVer2Application.java
@@ -0,0 +1,13 @@
+package io.npee.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBlogVer2Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/npee/blog/SpringbootBlogVer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.npee.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogVer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
